b2faeb5b3f24398e74c9da861d20906b493a6803
Strathcona <joe@envisageny.com>
2018-12-30 17:39:28 -0500

    Working Modal Popups

---
+++ Assets/Scripts/UI/ModalPopup.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModalPopup : MonoBehaviour {
+    public Text titleText;
+    public Text bodyText;
+    public GameObject optionsObject;
+    public GameObject optionButtonPrefab;
+    public List<GameObject> optionButtons = new List<GameObject>();
+    public ModalPopupManager popupManager;
+
+    private void Awake() {
+        if(optionButtonPrefab == null) {
+            optionButtonPrefab = Resources.Load("Prefabs/Button", typeof(GameObject)) as GameObject;
+        }
+    }
+
+    public void DisplayMessage(string title, string body, List<string> buttonLabels, List<Action> buttonActions) {
+        if(buttonLabels.Count != buttonActions.Count) {
+            Debug.LogError("Different number of labels and actions given to modal popup");
+            return;
+        }
+
+        titleText.text = title;
+        bodyText.text = body;
+
+        if(buttonLabels.Count > optionButtons.Count) {
+            int neededButtons = buttonLabels.Count - optionButtons.Count;
+            for(int i = 0; i < neededButtons; i++) {
+                GameObject g = GameObject.Instantiate(optionButtonPrefab);
+                g.transform.SetParent(optionsObject.transform);
+                optionButtons.Add(g);
+                g.SetActive(false);
+            }
+        }
+
+        Debug.Log(buttonActions.Count);
+        for(int j = 0; j < buttonLabels.Count; j++) {
+            optionButtons[j].SetActive(true);
+            optionButtons[j].GetComponentInChildren<Text>().text = buttonLabels[j];
+            var k = j;//apparently delegates don't pass by value exactly, so copy the value so it won't change
+            optionButtons[j].GetComponent<Button>().onClick.AddListener(delegate { buttonActions[k](); });
+            optionButtons[j].GetComponent<Button>().onClick.AddListener(delegate { popupManager.ClosePopup(this); });
+        }
+    }
+
+    private void CloseThis() {
+    }
+
+    public void Clear() {
+        titleText.text = "";
+        bodyText.text = "";
+        foreach(GameObject g in optionButtons) {
+            g.GetComponentInChildren<Text>().text = "";
+            g.GetComponent<Button>().onClick.RemoveAllListeners();
+            g.SetActive(false);            
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ModalPopupManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+public class ModalPopupManager : MonoBehaviour {
+    public static ModalPopupManager instance;
+    public GameObject modalPopupPrefab;
+    public List<GameObject> activePopups = new List<GameObject>();
+    public List<GameObject> inactivePopups = new List<GameObject>();
+
+    private void Awake() {
+        if(instance == null) {
+            instance = this;
+        } else {
+            Debug.LogError("You've put another modal popup manager somewhere...");
+        }
+
+        if (modalPopupPrefab == null) {
+            modalPopupPrefab = Resources.Load("Prefabs/Modal Popup", typeof(GameObject)) as GameObject;
+        }
+    }
+
+    public void DisplayModalPopup(string title, string body, List<string> buttonLabels, List<Action> buttonPopups) {
+        if(inactivePopups.Count < 1) {
+            GameObject g = GameObject.Instantiate(modalPopupPrefab, transform, true);
+            inactivePopups.Add(g);
+            RectTransform t = g.GetComponent<RectTransform>();
+            t.offsetMax = Vector2.zero;
+            t.offsetMin = Vector2.zero;
+            t.anchorMin = new Vector2(0.3f, 0.3f);
+            t.anchorMax = new Vector2(0.7f, 0.7f);
+            g.GetComponent<ModalPopup>().popupManager = this;
+            g.SetActive(false);
+        }
+
+        GameObject popup = inactivePopups[0];
+        activePopups.Add(popup);
+        inactivePopups.Remove(popup);
+        popup.SetActive(true);
+        popup.GetComponent<ModalPopup>().DisplayMessage(title, body, buttonLabels, buttonPopups);
+    }
+
+    public void ClosePopup(ModalPopup popup) {
+        popup.Clear();
+        inactivePopups.Add(popup.gameObject);
+        activePopups.Remove(popup.gameObject);
+        popup.gameObject.SetActive(false);
+    }
+}
